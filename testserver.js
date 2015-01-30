d115343c140b222620ab01d40671911b5b6697d9
MirHamzaHasan <MirHamzaHasan@users.noreply.github.com>
2015-01-30 21:37:22 +0500

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -1,26 +1,30 @@
 var http = require('http'), io = require('socket.io');
 
 // Start the server at port 8080
-
 var server = http.createServer(function(req, res){ 
 
     // Send HTML headers and message
     res.writeHead(200,{ 'Content-Type': 'text/html' }); 
     res.end('<h1>Hello Socket Lover!</h1>');
 });
-
-server.listen(process.env.PORT);
-
+server.listen(process.env.PORT,process.env.IP);
 // Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
-
+var socket = io.listen(server,function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+var port = process.env.PORT || 3000;
+server.listen(port,process.env.IP, function () {
+  var addr = server.address();
+  console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
 // Add a connect listener
 socket.on('connection', function(client){ 
 console.log("connectedd");
     // Create periodical which ends a message to the client every 5 seconds
     var interval = setInterval(function() {
         client.send('This is a message from the server!  ' + new Date().getTime());
-    },1000);
+    },5000);
 
     // Success!  Now listen to messages to be received
     client.on('message',function(event){ 
