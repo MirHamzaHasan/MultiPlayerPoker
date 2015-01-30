838e759f2998a568c0f460e6b26b0798c7681619
MirHamzaHasan <MirHamzaHasan@users.noreply.github.com>
2015-01-30 19:44:08 +0500

    Update testserver.js

--- testserver.js
+++ testserver.js
@@ -8,10 +8,7 @@ var server = http.createServer(function(req, res){
     res.writeHead(200,{ 'Content-Type': 'text/html' }); 
     res.end('<h1>Hello Socket Lover!</h1>');
 });
-io.configure (function () { 
-  io.set ("transports", ["xhr-polling"]); 
-  io.set ("polling duration", 10); 
-});
+
 server.listen(process.env.PORT);
 
 // Create a Socket.IO instance, passing it our server
